6e51cddbad5a8811fbbc2251ba1f9fdc23d99f32
Ilya <99236906+kitikov98@users.noreply.github.com>
2022-03-31 16:28:12 +0300

    Delete 24.01.2022.py

--- my_proj/24.01.2022.py
+++
@@ -1,4 +0,0 @@
-perv=int(input('введите первое число '))
-vtor= int(input('введите второе число '))
-print(f'{perv} плюс {vtor} равно {perv+vtor}')
-print('{0} плюс {1} равно {2}'.format(perv,vtor,perv+vtor))
