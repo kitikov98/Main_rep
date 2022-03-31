d2009d5bd581729e014e6337d5ef4fb441943ebd
Ilya <99236906+kitikov98@users.noreply.github.com>
2022-03-31 16:27:43 +0300

    Delete 20.01.2022(2).py

--- my_proj/20.01.2022(2).py
+++
@@ -1,4 +0,0 @@
-tovar = input('Введите название товара ')
-massa = int(input('Введите массу товара '))
-tsena=int(input('Введите стоимость товара за кг '))
-print(tovar, 'в количистве', massa, 'кг стоит',tsena*massa,'руб.')
